bf6440cfb6fd191657d30f4f231cbc9ef9db35b5
Vinicius Pimenta <viniciustavarespimenta@gmail.com>
2021-02-04 06:07:16 -0300

    Corrige erros relacionados ao script da aplicaçao

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(routes);
+
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello GoStack' }),
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
